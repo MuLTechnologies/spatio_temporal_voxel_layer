1723408c888dcb0c220a22ddef6397579f4bb37c
Jakub Klein <jakub.klein@husarion.com>
2025-06-18 14:24:27 +0200

    Revert "Added ClearEntireGridCallback"
    This reverts commit e1dbc17a438e7ddd973232e6dd1b7cfe097157f9.

--- include/spatio_temporal_voxel_layer/spatio_temporal_voxel_layer.hpp
+++ include/spatio_temporal_voxel_layer/spatio_temporal_voxel_layer.hpp
@@ -151,10 +151,6 @@ public:
     const std::shared_ptr<rmw_request_id_t>/*header*/, 
     std::shared_ptr<std_srvs::srv::Trigger::Request>, 
     std::shared_ptr<std_srvs::srv::Trigger::Response> resp);
-  void ClearEntireGridCallback(
-    const std::shared_ptr<rmw_request_id_t>/*header*/, 
-    std::shared_ptr<std_srvs::srv::Trigger::Request>, 
-    std::shared_ptr<std_srvs::srv::Trigger::Response> resp);
 
 private:
   // Sensor callbacks
@@ -209,7 +205,6 @@ private:
   rclcpp::Service<nav2_msgs::srv::ClearGridAroundPose>::SharedPtr _clear_grid_around_pose_srv;
   rclcpp::Service<std_srvs::srv::Trigger>::SharedPtr _save_stvl_map_srv;
   rclcpp::Service<std_srvs::srv::Trigger>::SharedPtr _erase_stvl_map_srv;
-  rclcpp::Service<std_srvs::srv::Trigger>::SharedPtr _clear_entire_grid_srv;
   std::unique_ptr<rclcpp::Duration> _map_save_duration;
   rclcpp::Time _last_map_save_time;
   std::string _stvl_map_file;
